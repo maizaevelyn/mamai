2e4f1341edb812c7e59318c36bf633c9276be803
maizaevelyn <161883419+maizaevelyn@users.noreply.github.com>
2024-04-30 16:50:00 -0300

    labelteste.py

---
+++ labelteste.py
@@ -0,0 +1,32 @@
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
+
+
+class RotuloApp(App):
+    def build(self):
+        layout = BoxLayout(orientation='vertical')
+        self.lab1 = Label = Label( 
+            text='SENAI', color=[0, 1, 0, 1],
+            font_size=40, bold=True
+
+        )
+
+        self.lab2 = Label(
+            text='SESI', color=[0, 1, 0, 1],
+            font_size=40, italic=True
+            
+        )
+
+        self.lab3 = Label(
+            text='ENEM', color=[0, 0, 1, 1],
+            font_size=40, font_name='Arieal',
+            underline=True
+        )
+        
+        layout.add_widget(self.lab1)
+        layout.add_widget(self.lab2)
+        layout.add_widget(self.lab3)
+
+if __name__ == '__main__':
+    RotuloApp() .run()
