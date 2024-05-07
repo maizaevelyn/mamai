f2bb72597b4694f69144a60a627c6ec41514a619
maizaevelyn <161883419+maizaevelyn@users.noreply.github.com>
2024-05-07 17:08:01 -0300

    checkbox.py

---
+++ checkbox.py
@@ -0,0 +1,10 @@
+from kivy.app import App
+from kivy.uix.button import Button 
+
+class MinhaApp(App):
+    def build(self):
+        return Button(text='Cilque Aqui', size_hint=(0.5, 0.2))
+    # Botão ocupará metade da largadura e 20% da altura do layout pai
+
+if __name__ == "__main__":
+    MinhaApp().run()
