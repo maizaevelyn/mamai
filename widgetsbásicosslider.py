7f1aeb99bf5f8e5efe248c45dc0d2c103fff73be
maizaevelyn <161883419+maizaevelyn@users.noreply.github.com>
2024-05-08 13:52:44 -0300

    widgetsbásicosslider.py

---
+++ widgetsbásicosslider.py
@@ -0,0 +1,9 @@
+from kivy.app import App
+from kivy.uix.slider import slider
+
+class MinhaApp(App):
+    def build(self):
+        return super().build(self):
+    
+if __name__ == "__main__":
+    MinhaApp().run()
